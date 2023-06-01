ddd2d990303b0ca7c12441259246c02b5fd250fe
XinDi Hu <HuXDev@pm.me>
2023-05-31 22:42:23 -0500

    set up dockerfile and k8s deploy files for auth

---
+++ auth/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello from root')
+})
+
+app.listen(3000, () => {
+    console.log('listen on port 3000')
+})
