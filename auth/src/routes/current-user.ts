36fad1ef1a5fefbc6cae1919755a9740366febc6
XinDi Hu <HuXDev@pm.me>
2023-06-10 17:38:56 -0500

    extract current user & auth require to their own middleware for code reuse

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
+import { currentUser } from '../middlewares/current-user'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        if (!req.session?.jwt) {
-            return res.send({ currentUser: null })
-        }
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-        res.send({ currentUser: payload })
-    } catch (err) {
-        res.send({ currentUser: null })
-        next(err)
+router.get(
+    '/api/users/currentuser',
+    currentUser,
+
+    (req: Request, res: Response, next: NextFunction) => {
+        res.send({ currentUser: req.currentUser || null })
+        next()
     }
-})
+)
 
 export { router as currentUserRouter }
