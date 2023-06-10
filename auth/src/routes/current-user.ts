b770ccba2e05630efd1d7366c6201c5e6a4641d3
XinDi Hu <HuXDev@pm.me>
2023-06-10 15:08:19 -0500

    added logic for currentUser & signout endpoints

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi from currentuser')
+router.get('/api/users/currentuser', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req.session?.jwt) {
+            return res.send({ currentUser: null })
+        }
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+        res.send({ currentUser: payload })
+    } catch (err) {
+        res.send({ currentUser: null })
+        next(err)
+    }
 })
 
 export { router as currentUserRouter }
